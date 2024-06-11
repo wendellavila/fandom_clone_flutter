06c79f9594a34dee46335f0f3a4df4dc61e5bf79
wendellavila <wendelljcavila@gmail.com>
2024-06-11 07:27:44 -0300

    refactor: Moving theme definition to separate file

---
+++ lib/config/theme_data.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData themeData({required BuildContext context, required ThemeMode themeMode}) {
+  ThemeData lightTheme = ThemeData(
+    iconTheme: const IconThemeData(color: Color(0XFF3A3A3A)),
+    textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Color(0XFF3A3A3A)),
+        bodyMedium: TextStyle(color: Color(0XFF3A3A3A)),
+        bodySmall: TextStyle(color: Color(0XFF3A3A3A))),
+    appBarTheme: const AppBarTheme(color: Color(0XFF001225)),
+    useMaterial3: false,
+    fontFamily: GoogleFonts.rubik().fontFamily,
+    colorScheme: Theme.of(context).colorScheme.copyWith(
+        primary: const Color(0XFF001225),
+        primaryContainer: const Color(0XFFf2f2f2),
+        onPrimary: Colors.white,
+        onPrimaryContainer: const Color(0XFF3A3A3A),
+        secondary: const Color(0XFFE77484),
+        onSecondary: const Color(0XFF3A3A3A),
+        tertiary: const Color(0XFF037ABF)),
+    scaffoldBackgroundColor: Colors.white,
+    listTileTheme: const ListTileThemeData(
+      iconColor: Color(0XFF3A3A3A),
+      textColor: Color(0XFF3A3A3A),
+    ),
+  );
+
+  ThemeData darkTheme = ThemeData(
+    iconTheme: const IconThemeData(color: Color(0XFFE6E6E6)),
+    textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Color(0XFFE6E6E6)),
+        bodyMedium: TextStyle(color: Color(0XFFE6E6E6)),
+        bodySmall: TextStyle(color: Color(0XFFE6E6E6))),
+    appBarTheme: const AppBarTheme(color: Color(0XFF037ABF)),
+    useMaterial3: false,
+    fontFamily: GoogleFonts.rubik().fontFamily,
+    colorScheme: Theme.of(context).colorScheme.copyWith(
+        primary: const Color(0XFF037ABF),
+        primaryContainer: const Color(0XFF263646),
+        onPrimary: Colors.white,
+        onPrimaryContainer: Colors.white,
+        secondary: const Color(0XFFB84D68),
+        onSecondary: const Color(0XFFE6E6E6),
+        tertiary: const Color(0XFF40A4FF)),
+    scaffoldBackgroundColor: const Color(0XFF001225),
+    listTileTheme: const ListTileThemeData(
+      iconColor: Colors.white,
+      textColor: Colors.white,
+    ),
+  );
+
+  return themeMode == ThemeMode.light ? lightTheme : darkTheme;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:fandom_clone/ui/screens/category/category_screen.dart';
 import 'package:fandom_clone/providers/theme_notifier.dart';
+import 'package:fandom_clone/config/theme_data.dart';
 
 void main() {
   runApp(
@@ -27,52 +27,8 @@ class _AppState extends ConsumerState<App> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FANDOM Clone',
-      theme: ThemeData(
-        iconTheme: const IconThemeData(color: Color(0XFF3A3A3A)),
-        textTheme: const TextTheme(
-            bodyLarge: TextStyle(color: Color(0XFF3A3A3A)),
-            bodyMedium: TextStyle(color: Color(0XFF3A3A3A)),
-            bodySmall: TextStyle(color: Color(0XFF3A3A3A))),
-        appBarTheme: const AppBarTheme(color: Color(0XFF001225)),
-        useMaterial3: false,
-        fontFamily: GoogleFonts.rubik().fontFamily,
-        colorScheme: Theme.of(context).colorScheme.copyWith(
-            primary: const Color(0XFF001225),
-            primaryContainer: const Color(0XFFf2f2f2),
-            onPrimary: Colors.white,
-            onPrimaryContainer: const Color(0XFF3A3A3A),
-            secondary: const Color(0XFFE77484),
-            onSecondary: const Color(0XFF3A3A3A),
-            tertiary: const Color(0XFF037ABF)),
-        scaffoldBackgroundColor: Colors.white,
-        listTileTheme: const ListTileThemeData(
-          iconColor: Color(0XFF3A3A3A),
-          textColor: Color(0XFF3A3A3A),
-        ),
-      ),
-      darkTheme: ThemeData(
-        iconTheme: const IconThemeData(color: Color(0XFFE6E6E6)),
-        textTheme: const TextTheme(
-            bodyLarge: TextStyle(color: Color(0XFFE6E6E6)),
-            bodyMedium: TextStyle(color: Color(0XFFE6E6E6)),
-            bodySmall: TextStyle(color: Color(0XFFE6E6E6))),
-        appBarTheme: const AppBarTheme(color: Color(0XFF037ABF)),
-        useMaterial3: false,
-        fontFamily: GoogleFonts.rubik().fontFamily,
-        colorScheme: Theme.of(context).colorScheme.copyWith(
-            primary: const Color(0XFF037ABF),
-            primaryContainer: const Color(0XFF263646),
-            onPrimary: Colors.white,
-            onPrimaryContainer: Colors.white,
-            secondary: const Color(0XFFB84D68),
-            onSecondary: const Color(0XFFE6E6E6),
-            tertiary: const Color(0XFF40A4FF)),
-        scaffoldBackgroundColor: const Color(0XFF001225),
-        listTileTheme: const ListTileThemeData(
-          iconColor: Colors.white,
-          textColor: Colors.white,
-        ),
-      ),
+      theme: themeData(context: context, themeMode: ThemeMode.light),
+      darkTheme: themeData(context: context, themeMode: ThemeMode.dark),
       themeMode: ref.watch(themeNotifier).theme,
       home: const CategoryPage(
         title: "Category:Browse",
